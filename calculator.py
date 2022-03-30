fea5aeae3c16959e9a785f602236b27ac4179788
Veronica Hall <86692664+ver0nicahall@users.noreply.github.com>
2022-03-30 17:15:57 -0500

    Implemented add and subtract

--- calculator.py
+++ calculator.py
@@ -10,5 +10,27 @@ print("The calculator has turned on.")
 
 #read input 
 while True:
-    user_input = input('What would you like to add?')
+    user_input = input('What would you like to perform? ')
+    tokens = user_input.split(' ')
+    operator = tokens[0]
+    num1 = tokens[1]
+    num2 = tokens[2]
+
+
+    print('User input: ' + str(user_input))
+    print('Tokens: ' + str(tokens))
+    print('operator: ' + str(operator))
+    print('num1: ' + str(num1))
+    print('num2: ' + str(num2))
+
+        #if the first token is q
+            #quit
+    #add
+    if operator == '+':
+        result = add(float(num1), float(num2))
     
+    if operator == '-':
+        result = subtract(float(num1), float(num2))
+    
+    #print result to screen
+    print('Result ' + str(result))
